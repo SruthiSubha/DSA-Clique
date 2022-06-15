ed3c6e935f6b56c9800271c45747b4f7af7f7eb6
Sruthi.S <54473621+SruthiSubha@users.noreply.github.com>
2022-06-15 09:24:48 +0530

    Add files via upload

--- Arrays/Index of Second Largest Element (In One Traversal).py
+++ Arrays/Index of Second Largest Element (In One Traversal).py
@@ -1,1 +1,10 @@
-
+array=[]
+nums=int(input())
+for i in range(nums):
+    array.append(int(input()))
+maximum=max(array)
+second=0
+for i in range(0,nums):
+    if array[i]!=maximum:
+        second=max(second,array[i])
+print(second)
